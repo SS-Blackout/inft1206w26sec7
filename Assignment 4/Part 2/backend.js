22a8b1174b0539713ed01e795e2e6093bbbed475
SS-Blackout <68345853+SS-Blackout@users.noreply.github.com>
2026-04-02 11:30:29 -0400

    added file headers

--- Assignment 4/Part 2/backend.js
+++ Assignment 4/Part 2/backend.js
@@ -1,3 +1,8 @@
+// Name: Noah Lad 100765390
+// File: backend.js
+// Date: April 10 2026
+// Description: Assignment 4 for INFT 1206-07
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
