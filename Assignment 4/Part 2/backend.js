3d990c2b394f34faf9f47b5356069cff999a1897
SS-Blackout <68345853+SS-Blackout@users.noreply.github.com>
2026-04-02 12:04:11 -0400

    Added updateDisplayedImage function and for loop

--- Assignment 4/Part 2/backend.js
+++ Assignment 4/Part 2/backend.js
@@ -10,6 +10,25 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 // Create an array of images
-const images = [
-  { filename: "pic1.jpg", alt: "Closeup of a human eye" }, { filename: "pic2.jpg", alt: "Rock that looks like a wave" }, { filename: "pic3.jpg", alt: "Purple and white pansies" }, { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" }, { filename: "pic5.jpg", alt: "Large moth on a leaf" }];
+const images = [{ filename: "pic1.jpg", alt: "Closeup of a human eye" }, { filename: "pic2.jpg", alt: "Rock that looks like a wave" }, { filename: "pic3.jpg", alt: "Purple and white pansies" }, { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" }, { filename: "pic5.jpg", alt: "Large moth on a leaf" }];
 
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+function updateDisplayedImage(e) {
+  displayedImage.src = e.target.src;
+  displayedImage.alt = e.target.alt;
+}
+
+for (const image of images) {
+  const newImage = document.createElement("img");
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt = image.alt;
+  newImage.tabIndex = "0";
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener("click", updateDisplayedImage);
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
+}
