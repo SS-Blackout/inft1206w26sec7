719231a26c84e9834148de598d9fc7b3dbc33d26
SS-Blackout <68345853+SS-Blackout@users.noreply.github.com>
2026-04-06 16:45:11 -0400

    Added sample code

--- Assignment 4/Part 3/main.js
+++ Assignment 4/Part 3/main.js
@@ -2,3 +2,9 @@
 // File: main.js
 // Date: April 10 2026
 // Description: Assignment 4 for INFT 1206-07
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
